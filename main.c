4277bc88748ea88e3581a4fa9bb2599ef02a4509
Kim Jones <K.Jones20@edu.salford.ac.uk>
2015-07-14 01:29:15 +0100

    Added gnuplot pipe, created new functions, read audio data into array
    Signed-off-by: Kim Jones <K.Jones20@edu.salford.ac.uk>

--- main.c
+++ main.c
@@ -4,17 +4,82 @@
 *****************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sndfile.h>
+
+/* Functions */
+int get_wav_data(void);
+int plot_wav();
 
 int main(void)
 {
+	int ret;
+
+	ret = get_wav_data();
+
+	return ret;
+}
+
+
+int get_wav_data(void) {
 	SF_INFO input_info;
 	input_info.format = 0;
+	short *wav_data;
+	unsigned long long ret;	
 
-	SNDFILE *input = sf_open("/home/kim/wav_samples/bach_partita_e_maj.wav",
-				SFM_READ, &input_info);
+	/* Open file */
+	SNDFILE *input = 
+		sf_open("/home/kim/wav_samples/mono_bach_partita_e_maj.wav",
+		SFM_READ, &input_info);
 
-	printf("Sample Rate: %d\nChannels: %d\nFormat: %d\nSections: %d\nSeekable: %d\n", input_info.samplerate, input_info.channels, input_info.format, input_info.sections, input_info.seekable);
+	/* Print header data */
+	printf("Frames: %llu\nSample Rate: %d\nChannels: %d\nFormat: 0x%X\n"
+		"Sections: %d\nSeekable: %d\n",(unsigned long long) 
+		input_info.frames, input_info.samplerate, input_info.channels, 
+		input_info.format, input_info.sections, input_info.seekable);
+
+	/* Store data in array */
+	wav_data = (short *) calloc(input_info.frames * input_info.channels, 
+		sizeof(short));
+
+	if (wav_data == NULL) {
+		printf("Memory allocation error\nExiting...\n");
+		sf_close(input);
+		return -1;
+	}
+
+	ret = sf_readf_short(input, wav_data, input_info.frames);		
+
+	if (ret != input_info.frames) {
+		printf("Error! Some frames not read\nExiting...");
+		free(wav_data);
+		sf_close(input);
+		return -1;
+	}
+	
+	ret = plot_wav(wav_data, input_info.channels, input_info.frames);	
+
+	free(wav_data);
+	sf_close(input);
+	return (int)ret;
+}
+
+
+int plot_wav(short **wav_data, int channels, sf_count_t frames)
+{
+	FILE *handle;
+
+	handle = popen("gnuplot -persistent", "w");
+	if (handle == NULL) {
+		printf("Pipe open error\nExiting...");
+		return -1;
+	}
+
+	fprintf(handle, "set term gif\n");
+	fprintf(handle, "set output \"./wav.gif\"\n");
+	fprintf(handle, "plot sin(x)/x\n");	//test
+	fprintf(handle, "set output\n");
+	pclose(handle);
 
 	return 0;
 }
