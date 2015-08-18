33e977c5d8620618d6b95cd2ef8dc64b63dd9213
Kim Jones <K.Jones20@edu.salford.ac.uk>
2015-08-18 21:40:33 +0100

    Added octave band filtering
    I have added octave band filtering using liquid.
    Currently stores output in a float array.
    Need to cast to short and test this.
    
    Signed-off-by: Kim Jones <K.Jones20@edu.salford.ac.uk>

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sndfile.h>
+#include <math.h>
 #include "kiss_fft.h"
+#include "liquid.h"
 #include <libswresample/swresample.h>
 #include <libavutil/channel_layout.h>
 
@@ -21,11 +23,45 @@
 /* Globals */
 int num_bands = 8;
 int octave_bands[] = {63, 125, 250, 500, 1000, 2000, 4000, 8000};
+unsigned int oct_filt_order = 5;
 int samp_freq_per_band[] = {3000, 3000, 3000, 3000, 3000, 6000, 12000, 24000};
+float butter_b[8][11] = {
+	{1.0e-05 * 0.0191, 0, 1.0e-05 * -0.0953, 0, 1.0e-05 * 0.1907, 0, 
+		1.0e-05 * -0.1907, 0, 1.0e-05 * 0.0953, 0, 1.0e-05 * -0.0191},
+	{1.0e-04 * 0.0511, 0, 1.0e-04 * -0.2553, 0, 1.0e-04 * 0.5105, 0, 
+		1.0e-04 * -0.5105, 0, 1.0e-04 * 0.2553, 0, 1.0e-04 * -0.0511},
+	{0.0001, 0, -0.0006, 0, 0.0013, 0, -0.0013, 0, 0.0006, 0, -0.0001},
+	{0.0026, 0, -0.0128, 0, 0.0257, 0, -0.0257, 0, 0.0128, 0, -0.0026},
+	{0.0419, 0, -0.2094, 0, 0.4189, 0, -0.4189, 0, 0.2094, 0, -0.0419},
+	{0.0419, 0, -0.2094, 0, 0.4189, 0, -0.4189, 0, 0.2094, 0, -0.0419},
+	{0.0419, 0, -0.2094, 0, 0.4189, 0, -0.4189, 0, 0.2094, 0, -0.0419},
+	{0.0419, 0, -0.2094, 0, 0.4189, 0, -0.4189, 0, 0.2094, 0, -0.0419}
+};
+float butter_a[8][11] = {
+	{1.0000, -9.6137, 41.6762, -107.2820, 181.6015, -211.2216, 170.9536, 
+		-95.0705, 34.7671, -7.5499, 0.7393},
+	{1.0000, -9.0799, 37.4091, -92.0798, 149.9398, -168.7663, 132.9719,
+		-72.4197, 26.0936, -5.6173, 0.5487},
+	{1.0000, -7.6107, 27.0569, -58.9960, 87.2298, -91.3052, 68.4995,
+		-36.3811, 13.1041, -2.8958, 0.2992},
+	{1.0000, -3.6281, 7.9585, -11.8968, 13.6277, -11.9992, 8.3692, -4.4707,
+		1.8221, -0.5004, 0.0848},
+	{1.0000, 4.3339, 8.2183, 9.6168, 8.4562, 5.9140, 3.0377, 1.0937,
+		0.3038, 0.0597, 0.0026},
+	{1.0000, 4.3339, 8.2183, 9.6168, 8.4562, 5.9140, 3.0377, 1.0937,
+                0.3038, 0.0597, 0.0026},
+	{1.0000, 4.3339, 8.2183, 9.6168, 8.4562, 5.9140, 3.0377, 1.0937,
+                0.3038, 0.0597, 0.0026},
+	{1.0000, 4.3339, 8.2183, 9.6168, 8.4562, 5.9140, 3.0377, 1.0937,
+                0.3038, 0.0597, 0.0026}
+};
 
 /* Functions */
 int get_wav_data(void);
 int plot_wav(short *wav_data, int channels, sf_count_t frames, int samprate);
+
+//float test_b[11];
+//float test_a[11];
 
 int main(void)
 {
@@ -39,9 +75,10 @@ int main(void)
 
 int get_wav_data(void) {
 	SF_INFO input_info;
-	short *wav_data;
+	short *wav_data, *oct_filt_data;
 	unsigned long long ret = 0;	
-	int i, ret2;
+	uint8_t *output_data;
+	int i, ret2, resamp_frames;
 
 	/* Open file */
 	input_info.format = 0;
@@ -88,7 +125,7 @@ int get_wav_data(void) {
 	av_opt_set_sample_fmt(resamp, "out_sample_fmt", AV_SAMPLE_FMT_S16, 0);
 
 	/* Octave band filtering */
-	for (i = 0; i < 1; i++) {
+	for (i = 0; i < num_bands; i++) {
 		av_opt_set_int(resamp, "out_sample_rate", 
 			samp_freq_per_band[i], 0); 
 
@@ -101,17 +138,35 @@ int get_wav_data(void) {
                 	return -1;
 		}
 
-		uint8_t *output;
-		int out_samples = av_rescale_rnd(swr_get_delay(resamp, 
+		/* Resample the audio */
+		resamp_frames = av_rescale_rnd(swr_get_delay(resamp, 
 			input_info.samplerate) + input_info.frames, 
 			samp_freq_per_band[i], input_info.samplerate, 
 			AV_ROUND_UP);
 
-		av_samples_alloc(&output, NULL, 1, out_samples, 
+		ret2 = av_samples_alloc(&output_data, NULL, 1, resamp_frames, 
 			AV_SAMPLE_FMT_S16, 0);
-		out_samples = swr_convert(resamp, &output, out_samples,
+
+		if (ret2 < 0) {
+			printf("Output buffer alloc failure\nExiting...\n");
+			free(wav_data);
+			sf_close(input);
+                	return -1;
+		}
+
+		resamp_frames = swr_convert(resamp, &output_data, resamp_frames,
 			(const uint8_t **) &wav_data, input_info.frames);
 
+		if (resamp_frames < 0) {
+			printf("Resampling failure\nExiting...\n");
+			av_freep(&output_data);
+			free(wav_data);
+			sf_close(input);
+                	return -1;
+		}
+
+#if 0
+		/* Generate wav to check resampled data is not garbage */
 		SF_INFO output_info;
 		output_info.samplerate = samp_freq_per_band[i];
 		output_info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
@@ -120,16 +175,46 @@ int get_wav_data(void) {
 		SNDFILE* otpt = sf_open("/home/kim/wav_samples/Z.wav", 
 			SFM_WRITE, &output_info);
 
+		sf_writef_short(otpt, (short *) output_data, resamp_frames);
 
-		sf_writef_short(otpt, (short *) output, out_samples);
-		
+		/* Filter coefficient calculation using Liquid */
+		liquid_iirdes(LIQUID_IIRDES_BUTTER,
+			LIQUID_IIRDES_BANDPASS, LIQUID_IIRDES_TF, 
+			oct_filt_order, 
+			octave_bands[i] / (sqrt(2) * samp_freq_per_band[i]),
+			octave_bands[i] / samp_freq_per_band[i], 1.0, 1.0,
+			test_b, test_a);		
+#endif		
+		/* Octave band filtering */
+		float *tmp = calloc(resamp_frames, 4);
+
+		if (tmp ==NULL) {
+			printf("Output buffer alloc failure\nExiting...\n");
+			av_freep(&oct_filt_data);
+			av_freep(&output_data);
+			free(wav_data);
+			sf_close(input);
+                	return -1;
+		}
+
+		iirfilt_rrrf filt_obj = iirfilt_rrrf_create(&butter_b[i][0], 
+			2 * oct_filt_order + 1, &butter_a[i][0], 
+			2 * oct_filt_order + 1);	
 
 
+		int j;
+		for (j = 0; j < resamp_frames; j++)
+			iirfilt_rrrf_execute(filt_obj, (float) output_data[j], 
+				&tmp[j]);
+
+		iirfilt_rrrf_destroy(filt_obj);
+
+		av_freep(&output_data);
 	}
 	
 
 	/* Hilbert Transform */
-	printf("DEBUG: Hilbert transform\n");	
+	printf("DEBUG: Hilbert transform\n");
 
 	
 
